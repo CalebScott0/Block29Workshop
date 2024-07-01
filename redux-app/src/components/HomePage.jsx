7dbc4266a7827ab4a0a8ef26e7c6511393966421
CalebScott0 <calebbenscott@gmail.com>
2024-07-01 17:18:16 -0500

    Finished app more or less

--- redux-app/src/components/HomePage.jsx
+++ redux-app/src/components/HomePage.jsx
@@ -1,3 +1,12 @@
 export default function HomePage() {
-    return <h1>Puppy Bowl Home Page</h1>
+  return (
+    <div className="home-page">
+      <h1>Welcome to this year's Puppy Bowl!</h1>
+      <p>
+        On the top of your screen you may click 'All Players' to view a roster
+        of all registered players.
+      </p>
+      <p>Or, you may click 'Add Player' to be directed to a signup form for new players.</p>
+    </div>
+  );
 }
